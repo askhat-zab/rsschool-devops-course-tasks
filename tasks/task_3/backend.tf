0d694723fc6c77c84d4834087324a32d5336b8c7
zaskhat <askhat@hp-envy-x360>
2024-11-04 05:33:45 +0500

    disable local envs

--- tasks/task_3/backend.tf
+++ tasks/task_3/backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "askhat-zab-tf-state"
-    key    = "task-3/terraform.tfstate"
+    key    = "task-4/terraform.tfstate"
     region = "eu-central-1"
   }
 }
--- tasks/task_4/backend.tf
+++ tasks/task_4/backend.tf
@@ -1,8 +1,8 @@
-# # connect s3 backend
-# terraform {
-#   backend "s3" {
-#     bucket = "askhat-zab-tf-state"
-#     key    = "task-4/terraform.tfstate"
-#     region = "eu-central-1"
-#   }
-# }
+# connect s3 backend
+terraform {
+  backend "s3" {
+    bucket = "askhat-zab-tf-state"
+    key    = "task-4/terraform.tfstate"
+    region = "eu-central-1"
+  }
+}
